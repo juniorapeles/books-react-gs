e1babd309114045248ab226241795fa034d60665
eli <elijunior.apeles2005@gmail.com>
2025-12-17 15:38:41 -0300

    feat: listbook

--- src/modules/books/presentation/components/ListBook.tsx
+++ src/modules/books/presentation/components/ListBook.tsx
@@ -15,13 +15,22 @@ export const ListBookGrid = () => {
       {books.map((book, index) => (
         <div
           key={index}
-          className="flex flex-col w-full h-56 justify-between p-3 border rounded-md hover:shadow-md transition-all"
+          className="flex flex-col overflow-hidden relative w-full h-56 justify-between p-3 border rounded-md hover:shadow-md transition-all"
         >
-          <div></div>
-          <p className="font-semibold uppercase text-base max-w-32">
-            {book.title}
-          </p>
-          <p className="text-xs text-zinc-400">{book.author}</p>
+          <div
+            className="absolute inset-0 z-0"
+            style={{
+              background: `linear-gradient(225deg, #FFB3D9 0%, #FFD1DC 20%, #FFF0F5 40%, #E6F3FF 60%, #D1E7FF 80%, #C7E9F1 100%)`,
+            }}
+          />
+
+          <div className="z-10 flex flex-col h-full justify-between">
+            <div></div>
+            <p className="font-semibold uppercase text-base max-w-32">
+              {book.title}
+            </p>
+            <p className="text-xs text-sky-500">{book.author}</p>
+          </div>
         </div>
       ))}
     </div>
